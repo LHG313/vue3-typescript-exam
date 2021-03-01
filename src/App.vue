65fcf59b4e8ee95e0c41bade13823f9640c6170d
LHG313 <dd31391@gmail.com>
2021-03-01 22:25:07 +0900

    필요없는 파일 제거

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, pariatur? Cumque alias, repellat blanditiis dignissimos molestias pariatur in voluptates ratione? Voluptatem minus, nisi obcaecati exercitationem debitis ab voluptas consectetur asperiores.
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+})
+</script>
+
+<style>
+html, body {
+  margin:0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>

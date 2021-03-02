50d1d29b00cbd34e132e0c038e7c0f511ad19a69
LHG313 <dd31391@gmail.com>
2021-03-02 13:25:05 +0900

    폰트적용

--- src/App.vue
+++ src/App.vue
@@ -34,14 +34,3 @@ export default defineComponent({
   name: 'App',
 })
 </script>
-
-<style>
-html, body {
-  margin:0;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-</style>

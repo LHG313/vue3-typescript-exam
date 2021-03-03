a51373f9b8e68934ab45a4098b888f4fe421fdc9
LHG313 <dd31391@gmail.com>
2021-03-03 22:56:54 +0900

    게시물 리스트의 쿼리, boardId에 따라 다른 게시판 게시물 노출

--- src/pages/ArticleListPage.vue
+++ src/pages/ArticleListPage.vue
@@ -21,70 +21,66 @@
 
   <section class="section section-article-list px-2">
     <div class="container mx-auto">
-  
-<div class="mt-6" v-bind:key="article.id" v-for="article in state.articles">
-  <div class="px-10 py-6 bg-white rounded-lg shadow-md">
-    <div class="flex justify-between items-center">
-      <span class="font-light text-gray-600">
-        2021-03-02 22:50:10
-      </span>
-      <a href="#" class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500">
-        자유
-        </a>
-  </div>
-  <div class="mt-2">
-    <a href="#" class="text-2xl text-gray-700 font-bold hover:underline">Build
-        {{article.title}}
-        </a>
-      <p class="mt-2 text-gray-600">
-        {{article.body}}
-      </p>
-  </div>
-  <div class="flex justify-between items-center mt-4">
-    <a href="#" class="text-blue-500 hover:underline">자세히 보기</a>
-    <div>
-      <a href="#" class="flex items-center">
-        <img src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=731&amp;q=80" alt="avatar" class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block">
-        <h1 class="text-gray-700 font-bold hover:underline">홍길동</h1>
-        </a>
+
+      <div class="mt-6" v-bind:key="article.id" v-for="article in state.articles">
+        <div class="px-10 py-6 bg-white rounded-lg shadow-md">
+          <div class="flex justify-between items-center">
+            <span class="font-light text-gray-600">
+              2021-02-24 10:20:30
+            </span>
+            <a href="#" class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500">
+              자유
+            </a>
+          </div>
+          <div class="mt-2">
+            <a href="#" class="text-2xl text-gray-700 font-bold hover:underline">
+              {{ article.title }}
+            </a>
+            <p class="mt-2 text-gray-600">
+              {{ article.body }}
+            </p>
+          </div>
+          <div class="flex justify-between items-center mt-4">
+            <a href="#" class="text-blue-500 hover:underline">자세히 보기</a>
+            <div>
+              <a href="#" class="flex items-center">
+                <img src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=731&amp;q=80" alt="avatar" class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block">
+                <h1 class="text-gray-700 font-bold hover:underline">홍길동</h1>
+              </a>
+            </div>
+          </div>
         </div>
-       </div>
       </div>
-  </div>
     </div>
   </section>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, reactive, getCurrentInstance, onMounted } from 'vue'
-import {useRoute} from 'vue-router'
-import {IArticle} from '../types/'
-import {MainApi} from '../apis/'
-
+import { useRoute } from 'vue-router'
+import { IArticle } from '../types/'
+import { MainApi } from '../apis/'
 export default defineComponent({
   name: 'ArticleListPage',
   setup() {
-    const route = useRoute()
+    const route = useRoute();
     const mainApi:MainApi = getCurrentInstance()?.appContext.config.globalProperties.$mainApi;
-
     const newArticleTitleElRef = ref<HTMLInputElement>();
     const newArticleBodyElRef = ref<HTMLInputElement>();
-
     const state = reactive({
       articles: [] as IArticle[]
     });
-
     function loadArticles(boardId:number) {
       mainApi.article_list(boardId)
       .then(axiosResponse => {
-        state.articles = axiosResponse.data.body.articles
+        state.articles = axiosResponse.data.body.articles;
       });
     }
-
     onMounted(() => {
-      loadArticles(2);
+      const boardId:number = parseInt(route.query.boardId as string ?? "1");
+       
+      loadArticles(boardId);
     });
-
     function checkAndWriteArticle() {
       if ( newArticleTitleElRef.value == null ) {
         return;
@@ -112,7 +108,7 @@ export default defineComponent({
       newArticleTitleEl.focus();
     }
     function writeArtile(title:string, body:string) {
-
+      
     }
     return {
       state,

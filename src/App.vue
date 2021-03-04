ac1c3da36dcf95d79aecf6f110db124f1bc682d4
LHG313 <dd31391@gmail.com>
2021-03-04 23:21:33 +0900

    postByForm 메서드를 통해서 백엔드와 호환되도록 처리

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,22 @@
         <ul class="flex h-full">
           <li>
             <router-link to="/" class="h-full flex items-center font-bold px-4 hover:bg-white hover:text-black">
-              HOME
+              홈
             </router-link>
           </li>
           <li>
-            <router-link to="/article/list" class="h-full flex items-center font-bold px-4 hover:bg-white hover:text-black">
-              ARTICLE LIST
+            <router-link to="/article/write" class="h-full flex items-center font-bold px-4 hover:bg-white hover:text-black">
+              글 작성
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/article/list?boardId=1" class="h-full flex items-center font-bold px-4 hover:bg-white hover:text-black">
+              공지사항 리스트
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/article/list?boardId=2" class="h-full flex items-center font-bold px-4 hover:bg-white hover:text-black">
+              자유 리스트
             </router-link>
           </li>
         </ul>
@@ -35,44 +45,26 @@ export default defineComponent({
 })
 </script>
 
-<style>
-html, body {
-  margin:0;
-}
-
-html {
-  font-family: 'LotteMartDream', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-</style>
-
 <style lang="postcss">
 .btn-primary {
   @apply py-2 px-4 bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg;
 }
-
 .btn-secondary {
   @apply py-2 px-4 bg-gray-600 hover:bg-gray-700 focus:ring-gray-500 focus:ring-offset-gray-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg;
 }
-
 .btn-success {
   @apply py-2 px-4 bg-green-600 hover:bg-green-700 focus:ring-green-500 focus:ring-offset-green-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg;
 }
-
 .btn-danger {
   @apply py-2 px-4 bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg;
 }
-
 .btn-warning {
   @apply py-2 px-4 bg-yellow-600 hover:bg-yellow-700 focus:ring-yellow-500 focus:ring-offset-yellow-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg;
 }
-
 .btn-info {
   @apply py-2 px-4 bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg;
 }
-
-.btns > [class*="btn-"] + [class*="btn-"]{
+.btns > [class*="btn-"] + [class*="btn-"] {
   margin-left: theme('spacing.2');
 }
 </style>
